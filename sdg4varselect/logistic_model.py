5b19a971c9b323f7c206aee946cabeb6b755a38a
caillebotteAntoine <caillebotte.antoine@gmail.com>
2023-02-23 11:08:07 +0100

    push avant joint model

--- sdg4varselect/logistic_model.py
+++ sdg4varselect/logistic_model.py
@@ -1,79 +1,42 @@
 import jax.numpy as jnp
 import numpy as np
-from jax import jacrev, jit, grad
-
-from sdg4varselect import gaussian_prior, logistic_curve
-from sdg4varselect.miscellaneous import namedTheta
+from jax import jacrev, jit
 
 
-# ==== partial_loss ==== #
+"""
+import parametrization_cookbook.jax as pc
+
+parametrization = pc.NamedTuple(
+    beta1=pc.RealPositive(scale=100),
+    beta2=pc.Real(loc=100, scale=100),
+    beta3=pc.RealPositive(scale=100),
+    gamma2_1=pc.RealPositive(scale=100),
+    gamma2_2=pc.RealPositive(scale=100),
+)
+"""
+
+
+def logistic_curve_float(
+    x, supremum: float, midpoint: float, growth_rate: float
+) -> np.ndarray:
+    return supremum / (1 + np.exp(-(x - midpoint) / growth_rate))
+
+
 @jit
-def loss_without_prior(theta, Y, time, phi1, phi2, phi3) -> jnp.ndarray:
-    pred = logistic_curve(time, phi1, phi2, phi3)
-    out = jnp.sum(jnp.power(Y - pred, 2))
-    return jnp.sum(-out / (2 * theta.sigma2))
+def logistic_curve(
+    time: jnp.ndarray,  # shape = (J,) [None, :]
+    supremum: jnp.ndarray,  # shape = (N,) [:,None]
+    midpoint: jnp.ndarray,  # shape = (N,) [:,None]
+    growth_rate: float,
+) -> jnp.ndarray:  # shape = (N,J)
 
-
-@jit
-def loss_without_prior_array(theta, Y, time, phi1, phi2, phi3):
-    out = [
-        loss_without_prior(theta, Y[i], time, phi1[i], phi2[i], phi3[i])
-        for i in range(len(phi1))
-    ]
-    return jnp.array(out)
-
-
-# ==== Loss_array ==== #
-@jit
-def loss_array(theta, Y, time, phi1, phi2, phi3):
-    latent_prior = (
-        gaussian_prior(phi1, theta.beta1, theta.gamma2_1)
-        + gaussian_prior(phi2, theta.beta2, theta.gamma2_2)
-        + gaussian_prior(phi3, theta.beta3, 1)
+    return supremum[:, None] / (
+        1 + jnp.exp(-(time[None, :] - midpoint[:, None]) / growth_rate)
     )
 
-    return latent_prior + loss_without_prior_array(theta, Y, time, phi1, phi2, phi3)
-
-
-jac_loss = jit(jacrev(loss_array))
-
 
 @jit
-def loss(theta, Y, time, phi1, phi2, phi3):
-    out = loss_array(theta, Y, time, phi1, phi2, phi3)
-    return out.mean()
-
-
-grad_loss = jit(grad(loss))
-
-
-# === just for compilation and test ===
-def model(time, phi1, phi2, phi3, **kwargs):
-    N = len(phi1)
-    out = [logistic_curve(time, phi1[i], phi2[i], phi3[i]) for i in range(N)]
-    return jnp.array(out)
-
-
-if __name__ == "__main__":
-
-    # ==== Data simulation ==== #
-    N, J = 500, 200
-
-    eps = np.random.normal(0, np.sqrt(100), (N, J))
-    sim = {
-        "time": np.linspace(100, 1500, num=J),
-        "phi1": np.random.normal(200, np.sqrt(40), N),
-        "phi2": np.random.normal(500, np.sqrt(100), N),
-        "phi3": np.array([150 for i in range(N)]),
-    }
-
-    sim["Y"] = model(**sim) + eps
-
-    theta, thetaType = namedTheta(
-        beta1=np.array([300.0]),
-        gamma2_1=np.array([30.0]),
-        beta2=np.array([400.0]),
-        gamma2_2=np.array([30.0]),
-        beta3=np.array([200.0]),
-        sigma2=np.array([10.0]),
-    )
+def gaussian_prior(data, mean, variance) -> jnp.ndarray:
+    # Computation of the current target distrubtion score
+    out = jnp.log(2 * jnp.pi * variance) + jnp.power(data - mean, 2) / variance
+    return -out / 2
