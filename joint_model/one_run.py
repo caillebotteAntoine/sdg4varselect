e7054e46be5e3d5f489984af16c98a46a9a47da1
caillebotteAntoine <caillebotte.antoine@gmail.com>
2023-06-16 11:16:53 +0200

    new commit

---
+++ joint_model/one_run.py
@@ -0,0 +1,88 @@
+from sample import get_solver
+
+from sample import get_parametrization, get_sample_and_solver
+import numpy as np
+from sdg4varselect import jnp, jrd
+from model import jac_likelihood
+import sdg4varselect.plot as sdgplt
+
+DIM_COV = 10
+N_IND = 100
+
+parametrization, params_star_weibull = get_parametrization(DIM_COV)
+
+params0 = {
+    "mu1": 0.5,  # 1
+    "mu2": 50.0,  # 2
+    "mu3": 3.0,  # 3
+    "gamma2_1": 0.00025,  # 4
+    "gamma2_2": 2.0,  # 5
+    "sigma2": 0.0001,  # 6
+    "alpha": 5.0,  # 7
+    "beta": np.random.uniform(-1, 1, size=DIM_COV),
+}
+
+fisher_mask = (
+    jnp.arange(0, len(params0) + DIM_COV - 1) < len(params0) - 2
+)  # -2 car on veut pas de 2 paramètre beta and alpha
+# fisher_mask = jnp.array([True for i in range(len(fisher_mask))])
+
+kwargs_run_GD = {
+    "prox_regul": 1.29e-3,
+    "proximal_operator": True,
+}
+
+
+def estim(solver):
+    return solver.stochastic_gradient(
+        jac_likelihood=jac_likelihood,
+        fisher_preconditionner=True,
+        fisher_mask=fisher_mask,
+        smart_start=1000,
+        p=DIM_COV,
+        niter=2000,
+        **kwargs_run_GD,
+    )
+
+
+def sample_and_estim(params0, prng_key, verbatim=False):
+    solver, _, key = get_sample_and_solver(
+        parametrization,
+        prng_key,
+        params0,
+        params_star_weibull,
+        N_IND,
+        DIM_COV,
+        plateau_start=1000,
+        plateau_stop=1000 + 100,
+    )
+
+    solver.verbatim = verbatim
+    res = estim(solver)
+
+    return res, solver, key
+
+
+if __name__ == "__main__":
+    res, solver, _ = sample_and_estim(params0, jrd.PRNGKey(0), verbatim=True)
+
+    params_star_stack = jnp.hstack(
+        [
+            params_star_weibull.mu1,
+            params_star_weibull.mu2,
+            params_star_weibull.mu3,
+            params_star_weibull.gamma2_1,
+            params_star_weibull.gamma2_2,
+            params_star_weibull.sigma2,
+            params_star_weibull.alpha,
+            params_star_weibull.beta,
+        ]
+    )
+
+    sdgplt.plot_params(
+        x=res.theta,
+        x_star=np.array(params_star_stack),
+        p=DIM_COV,
+        names=solver.params_names,
+        logscale=False,
+    )
