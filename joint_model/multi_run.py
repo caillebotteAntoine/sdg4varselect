8733ee74cab6836ce3cfe911f4f4735138288ab2
Antoine Caillebotte <acaillebotte@erable>
2023-06-17 18:47:11 +0200

    csv

--- joint_model/multi_run.py
+++ joint_model/multi_run.py
@@ -105,7 +105,7 @@ print(bic_res)
 # ====================================================== #
 # ====================== INFERENCE ===================== #
 # ====================================================== #
-lbd_selection = lbd_set[bic_res["bic"] == bic_res["min"]] * 1.1
+lbd_selection = lbd_set[bic_res["bic"] == bic_res["min"]] * 1.7
 print(f"regularization value selected = {lbd_selection}")
 kwargs_run_GD["prox_regul"] = lbd_selection
 
