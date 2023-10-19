0c349139bcfccb79ae1aafeca5deef94d121ac09
caillebotteAntoine <caillebotte.antoine@gmail.com>
2023-10-19 10:07:21 +0200

    zut

--- joint_model_200/plot_multi_res.py
+++ joint_model_200/plot_multi_res.py
@@ -71,38 +71,36 @@ ax.set_title("regularization parameter")
 # ====================================================== #
 
 
-i = 1
+for i in range(len(bic)):
+    fig, axs = sdgplt.plot_regularization_path(theta_reg[i], lbd_set, bic[i])
+    ax, ax_bic = axs
 
-fig, axs = sdgplt.plot_regularization_path(theta_reg[i], lbd_set, bic[i])
-ax, ax_bic = axs
+    z = np.poly1d(np.polyfit(lbd_set, bic[i], deg=4))
 
-z = np.poly1d(np.polyfit(lbd_set, bic[i], deg=4))
+    lbd_set_ext = np.linspace(min(lbd_set), max(lbd_set), num=1000)
+    bic_approx = z(lbd_set_ext)
+    ax_bic.plot(lbd_set_ext, bic_approx, linewidth=4, color="k")
 
-lbd_set_ext = np.linspace(min(lbd_set), max(lbd_set), num=1000)
-bic_approx = z(lbd_set_ext)
-ax_bic.plot(lbd_set_ext, bic_approx, linewidth=4, color="k")
+    id = bic_approx.argmin()
+    ax_bic.axvline(
+        x=lbd_set_ext[id],
+        color="k",
+        linewidth=2,
+        linestyle="--",
+        label=r"$\lambda$ approx",
+    )
+    ax_bic.text(
+        lbd_set_ext[id],
+        0.8 * bic[i].max() + 0.2 * bic[i].min(),
+        rf"$\lambda$ = {lbd_set_ext[id]:.3e}",
+        ha="center",
+        va="center",
+        rotation="vertical",
+        backgroundcolor="white",
+    )
 
-id = bic_approx.argmin()
-ax_bic.axvline(
-    x=lbd_set_ext[id],
-    color="k",
-    linewidth=2,
-    linestyle="--",
-    label=r"$\lambda$ approx",
-)
-ax_bic.text(
-    lbd_set_ext[id],
-    0.8 * bic[i].max() + 0.2 * bic[i].min(),
-    rf"$\lambda$ = {lbd_set_ext[id]:.3e}",
-    ha="center",
-    va="center",
-    rotation="vertical",
-    backgroundcolor="white",
-)
-
-
-ax_ebic = ax.twinx()
-ax_ebic.plot(lbd_set, ebic[i], color="r", linewidth=2, linestyle="--", label="eBIC")
-id_min = np.nanargmin(ebic[i])
-sdgplt.plot_axvline(ax_ebic, lbd_set, ebic[i], id_min, color="g", msg="min(eBIC)")
-ax_ebic.legend(loc="upper right")
+    ax_ebic = ax.twinx()
+    ax_ebic.plot(lbd_set, ebic[i], color="r", linewidth=2, linestyle="--", label="eBIC")
+    id_min = np.nanargmin(ebic[i])
+    sdgplt.plot_axvline(ax_ebic, lbd_set, ebic[i], id_min, color="g", msg="min(eBIC)")
+    ax_ebic.legend(loc="upper right")
