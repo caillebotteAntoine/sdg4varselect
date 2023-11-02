2d3bba919f7d0a9f047a87581e136847bbbb4db7
caillebotteAntoine <caillebotte.antoine@gmail.com>
2023-11-02 15:09:10 +0100

    zut

--- joint_model/script.sh
+++ joint_model/script.sh
@@ -3,6 +3,6 @@
 
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
-poetry run python3 multi_res.py
+poetry run python3 one_res.py
 
 
