b6865f14e569bd4df1fca3a99479898d5a2874a8
Antoine Caillebotte <acaillebotte@erable>
2023-11-05 22:36:02 +0100

    csv

--- run_script/script.sh
+++ run_script/script.sh
@@ -4,7 +4,7 @@ systemd-run --user --scope -p CPUQuota=25%
 
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
-poetry run python3 run_200_C.py
+poetry run python3 run_400.py
 
 
 
