1601e7f4eee22bf375ef0818ddeca660dfccd9e5
caillebotteAntoine <caillebotte.antoine@gmail.com>
2024-02-01 13:37:37 +0100

    run_test

--- results/script.sh
+++ results/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#systemd-run --user --scope -p CPUQuota=25% 
+systemd-run --user --scope -p CPUQuota=25% 
 
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
