4f57b8ced00e600197077d3f6b6d370092b265c7
caillebotteAntoine <caillebotte.antoine@gmail.com>
2024-02-01 13:30:17 +0100

    script

--- results/script.sh
+++ results/script.sh
@@ -5,6 +5,6 @@
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
 export XLA_FLAGS="--xla_force_host_platform_device_count=<30>"
-#poetry run python3 run_test.py
-jupyter nbconvert --to html new_regularization_path.ipynb
+poetry run python3 run_test.py
+#jupyter nbconvert --to html new_regularization_path.ipynb
 
