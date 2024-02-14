13437e5ba8a53f629d6430e99ea07f81a68226cb
caillebotteAntoine <caillebotte.antoine@gmail.com>
2024-02-14 16:11:31 +0100

    script

--- results/script.sh
+++ results/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-systemd-run --user --scope -p CPUQuota=25% 
+systemd-run --user --scope -p CPUQuota=25% poetry run python3 run_test.py
 
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
-export XLA_FLAGS="--xla_force_host_platform_device_count=<30>"
-poetry run python3 run_test.py
+#export XLA_FLAGS="--xla_force_host_platform_device_count=<30>"
+#poetry run python3 run_test.py
 #jupyter nbconvert --to html new_regularization_path.ipynb
 
