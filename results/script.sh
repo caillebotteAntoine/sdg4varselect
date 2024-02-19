c9293351c518a82f99af9ebe64497fb4016143b0
caillebotteAntoine <caillebotte.antoine@gmail.com>
2024-02-19 15:43:01 +0100

    new script

--- results/script.sh
+++ results/script.sh
@@ -2,7 +2,11 @@
 
 systemd-run --user --scope -p CPUQuota=1000% 
 
-poetry run python3 run_test.py
+seed=$((SLURM_ARRAY_TASK_ID))
+
+poetry run python3  running_test_from_script.py $seed
+
+#run_test.py
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
 #export XLA_FLAGS="--xla_force_host_platform_device_count=<30>"
