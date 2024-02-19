8faa0df7ede198c884a29025170d4dc9ac9b873c
caillebotteAntoine <caillebotte.antoine@gmail.com>
2024-02-19 10:33:21 +0100

    run_test

--- results/script.sh
+++ results/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-systemd-run --user --scope -p CPUQuota=25% poetry run python3 run_test.py
+systemd-run --user --scope -p CPUQuota=1000% 
 
+poetry run python3 run_test.py
 ## Pour lancer mon code (code qui parallélise lui même)
 #python3 $source"sdg4varselect/joint_model/multi_run.py"
 #export XLA_FLAGS="--xla_force_host_platform_device_count=<30>"
